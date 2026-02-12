686daeef82e1d97584da5b7dc7d0d10df9fef155
Safirl <loic.leforestier@mmibordeaux.com>
2026-02-12 11:59:33 +0100

    added drag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './reset.css'
 import './style.css'
 import { fruits, type fruit } from './data/fruits'
+import gsap from "gsap";
 
 let appContainer = document.getElementById('app')
 if (!appContainer) {
@@ -15,7 +16,13 @@ document.addEventListener("mousemove", (ev: MouseEvent) => {
   mouseY = ev.clientY
 })
 
-const fruitDivs: { isDragged: boolean, div: HTMLDivElement }[] = []
+document.addEventListener("mouseup", () => {
+  fruitDivs.forEach((fruitDiv) => {
+    fruitDiv.isDragged = false
+  })
+})
+
+const fruitDivs: { distX: number, distY: number, isDragged: boolean, div: HTMLDivElement }[] = []
 
 const createFruit = (fruit: fruit) => {
   if (!appContainer) return;
@@ -29,7 +36,7 @@ const createFruit = (fruit: fruit) => {
   div.style.top = fruit.y.toString() + "px"
   div.addEventListener("pointerdown", (ev: PointerEvent) => onFruitDragStart(div, ev))
   div.addEventListener("pointerup", (ev: PointerEvent) => onFruitDragEnd(div, ev))
-  fruitDivs.push({ isDragged: false, div })
+  fruitDivs.push({ distX: 0, distY: 0, isDragged: false, div })
 }
 
 fruits.forEach((fruit: fruit) => {
@@ -42,15 +49,36 @@ const onFruitDragStart = (div: HTMLDivElement, ev: PointerEvent) => {
   const relatedFruitDiv = fruitDivs.find((fruitDiv) => div === fruitDiv.div)
   if (!relatedFruitDiv) return;
   const fruitDivLeft = Number(relatedFruitDiv.div.style.left.replace('px', ''))
-  const fruitDivRight = Number(relatedFruitDiv.div.style.right.replace('px', ''))
-  const distX = fruitDivLeft - 
+  const fruitDivTop = Number(relatedFruitDiv.div.style.top.replace('px', ''))
+  const distX = Math.abs(fruitDivLeft - mouseX)
+  const distY = Math.abs(fruitDivTop - mouseY)
+  relatedFruitDiv.isDragged = true
+  relatedFruitDiv.distX = distX
+  relatedFruitDiv.distY = distY
 }
 
 const onFruitDragEnd = (div: HTMLDivElement, ev: PointerEvent) => {
-
+  const relatedFruitDiv = fruitDivs.find((fruitDiv) => div === fruitDiv.div)
+  if (!relatedFruitDiv) return;
+  relatedFruitDiv.isDragged = false
+  relatedFruitDiv.distX = 0
+  relatedFruitDiv.distY = 0
 }
 
 const update = () => {
   requestAnimationFrame(update)
-
+  fruitDivs.forEach((fruitDiv) => {
+    if (fruitDiv.isDragged) {
+      gsap.to(fruitDiv.div.style, {
+        left: fruitDiv.div.style.left = (mouseX - fruitDiv.distX).toString() + 'px'
+      })
+      gsap.to(fruitDiv.div.style, {
+        top: fruitDiv.div.style.top = (mouseY - fruitDiv.distY).toString() + 'px'
+      })
+      fruitDiv.div.style.left = (mouseX - fruitDiv.distX).toString() + 'px'
+      fruitDiv.div.style.top = (mouseY - fruitDiv.distY).toString() + 'px'
+    }
+  })
 }
+
+update();
